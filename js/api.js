517a5012b5bb8cfc2b016d11a8b642aa9fc1fc9a
rumankazi62 <rumankazi629@gmail.com>
2021-09-02 13:46:29 +0600

    new commit

---
+++ js/api.js
@@ -0,0 +1,48 @@
+
+document.getElementById('error-message').style.display = 'none';
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // clear data
+    searchField.value = '';
+    document.getElementById('error-message').style.display = 'none';
+    if (searchText === '') {
+        // please write something to display
+    }
+    else {
+        // load data
+        const url = `https://openlibrary.org/search.json?q=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.docs))
+            .catch(error => displayError(error));
+    }
+}
+
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
+}
+
+const displaySearchResult = books => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    // if (books.length == 0) {
+        
+    // }
+    books.forEach(book => {
+        // console.log(book);
+        const imgUrl = `https://covers.openlibrary.org/b/id/{cover_i}-M.jpg`;
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        
+            <div class="card-body">
+                <img src="${book.cover_i}">
+                <h5 class="card-title">Author name: ${book.author_name[0]}</h5>
+                <p class="card-text">First published: ${book.first_publish_year}</p>
+            </div>
+        
+        `;
+        searchResult.appendChild(div);
+    })
+}
